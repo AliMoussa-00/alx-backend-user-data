7c84013f7a7f967bc96026614fd147324ff63bf2
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-16 17:32:05 +0000

    fix get_db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -68,7 +68,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD', 'root'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
+        database=os.getenv('PERSONAL_DATA_DB_NAME', '')
     )
 
     return connection
